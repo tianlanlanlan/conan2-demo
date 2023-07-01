f4bbf22c032678e831c8a34d9b26314e79c51a33
tianlan <tianlan01.wang>
2023-07-01 19:36:21 +0800

    add some logs

--- build.sh
+++ build.sh
@@ -5,24 +5,22 @@ set -e
 platform_options="<linux | riscv | arm>"
 recommand_conan_version="2.0.6"
 
-if [ $# -ne 1 ]; then
-  printf "usage:\n\t$0 $platform_options\n"
-  exit 1
-fi
-
 check_conan() {
   [ -z $(command -v conan) ] && echo "conan not installed" && exit 1
   if [ ! $(conan --version | grep -o '[0-9.]*') -eq "$conan_version" ]; then
-    pip install --force-reinstall -v conan==$conan_version
+    pip install conan==$conan_version --upgrade
   fi
 }
 
-# check_conan
+usage() {
+  printf "usage:\n\t$0 $platform_options\n"
+  exit 1
+}
 
-platform=$1
+[ $# -eq 1 ] && platform=$1 || platform="linux"
 case $platform in
 linux)
-  host_profile="default"
+  host_profile="./profiles/default"
   ;;
 riscv)
   host_profile="./profiles/riscv"
@@ -32,24 +30,34 @@ arm)
   ;;
 *)
   echo "unkown '$platform', use $platform_options"
-  exit 1
+  usage
   ;;
 esac
 
 # Debug | RelWithDebInfo | MinSizeRel | Release
-build_type="RelWithDebInfo"
+build_type="Debug"
 cmake_preset_prefix=$platform
 cmake_preset_suffix=$(echo $build_type | awk '{print tolower($0)}')
 cmake_preset="$cmake_preset_prefix-$cmake_preset_suffix"
 build_dir="build_$platform"
 
-rm -rf $build_dir && mkdir $build_dir
+rm -rf $build_dir
 conan install conanfile.py \
   --output-folder $build_dir \
   --profile $host_profile \
   --settings build_type=$build_type \
-  --options *:platform=$platform \
   --options *:shared=True \
+  --options *:platform=$platform \
   --build missing
 cmake --preset $cmake_preset --log-level=VERBOSE
 cmake --build --preset $cmake_preset
+
+# post process
+case $platform in
+linux)
+  ./$build_dir/bin/main
+  ;;
+riscv) ;;
+arm) ;;
+*) ;;
+esac
--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps
+from conan.tools import CppInfo
+from conan.tools.layout import basic_layout
 import os
 
 
@@ -13,8 +15,10 @@ class Conan2DemoRecipe(ConanFile):
     }
 
     def requirements(self):
-        self.requires("zlib/1.2.11")
+        self.requires("zlib/1.2.13")
         self.requires("fmt/9.1.0")
+        self.requires("protobuf/3.21.9")
+        self.requires("nlohmann_json/3.11.2")   # header only
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -23,9 +27,28 @@ class Conan2DemoRecipe(ConanFile):
         dep = CMakeDeps(self)
         dep.generate()
 
+        print("------------deps info------------")
+        aggregated_cpp_info = CppInfo(self)
+        deps = self.dependencies.host.topological_sort
+        deps = [dep for dep in reversed(deps.values())]
+        for dep in deps:
+            print(dep)
+            dep_cppinfo = dep.cpp_info.aggregated_components()
+            aggregated_cpp_info.merge(dep_cppinfo)
+            for cname, c in dep_cppinfo.components.items():
+                print(f"name: {cname}")
+        print(f"includedirs:")
+        [print(f"\t{d}") for d in aggregated_cpp_info.includedirs]
+        print(f"libdirs:")
+        [print(f"\t{d}") for d in aggregated_cpp_info.libdirs]
+        print(f"bindirs:")
+        [print(f"\t{d}") for d in aggregated_cpp_info.bindirs]
+        print(f"libs: {aggregated_cpp_info.libs}")
+        print(f"system_libs: {aggregated_cpp_info.system_libs}")
+
     def build_requirements(self):
         self.tool_requires("cmake/3.24.4")
 
     def layout(self):
-        self.folders.source = "."
-        self.folders.generators = "generators"
+        self.folders.generators = "conan"
+        # basic_layout(self)
